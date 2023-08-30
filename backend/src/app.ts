a461f27c0162cd98e88d062d67807fe0d10b99a6
Maruf <thesharifi.maruf@gmail.com>
2023-08-30 14:31:13 +0430

    app config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,34 +1,33 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import apiRoutes from "./routes/api";
-import { move } from "./controllers/move";
-import { initializeSocket } from "./socket-config";
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import apiRoutes from './routes/api'
+import { initializeSocket } from './socket-config'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT || 3001;
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost/tic-tac-toe";
+const app = express()
+const port = process.env.PORT || 3001
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/tic-tac-toe'
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use("/api", apiRoutes);
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api', apiRoutes)
 
 // Connect to MongoDB
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI)
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => {
+  console.log('Connected to MongoDB')
+})
 
-const server = initializeSocket(app);
+const server = initializeSocket(app)
 
 server.listen(port, () => {
-  // move();
-});
+  console.log('server is running on port ', port)
+})
