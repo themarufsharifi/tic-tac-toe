800ce52f903511fd390f03809fc1b4e624e90d92
Maruf <thesharifi.maruf@gmail.com>
2023-08-30 15:56:44 +0430

    made winner responsive

--- frontend/src/components/winner-modal.tsx
+++ frontend/src/components/winner-modal.tsx
@@ -11,7 +11,7 @@ interface GameProps {
 export const WinnerModal: FC<GameProps> = ({ isDraw, winner, handleQuitGame, handleNewGame }) => {
   return (
     <div className="bg-gray-900/90 z-10 min-h-screen w-full absolute top-0 left-0">
-      <div className="w-[500px] h-[250px] rounded-xl bg-[#1f3540] space-y-10 px-6 py-4 mx-auto mt-52 flex items-center justify-center flex-col">
+      <div className="w-[calc(100vw-16px)] md:w-[500px] h-[250px] rounded-xl bg-[#1f3540] space-y-10 px-6 py-4 mx-auto mt-52 flex items-center justify-center flex-col">
         <h2 className="flex flex-col items-center justify-center space-y-6 text-2xl md:text-4xl font-bold">
           {isDraw ? (
             <>
@@ -26,7 +26,7 @@ export const WinnerModal: FC<GameProps> = ({ isDraw, winner, handleQuitGame, han
           )}
         </h2>
 
-        <div className="flex items-center justify-center space-x-16">
+        <div className="flex items-center justify-center space-x-4">
           <button
             onClick={handleQuitGame}
             className="button px-4 rounded-md py-1 bg-[#a8bdc8] hover:bg-[#718087] hover:ring-4 hover:ring-gray-400"
